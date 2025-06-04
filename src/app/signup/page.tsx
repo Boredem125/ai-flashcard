2560778da8ff420b78ee552499c9d9590b878dbe
Aryan Hundia <aanunonu@gmail.com>
2025-06-04 19:10:00 +0000

    GOT 404

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+// This page no longer exists.
+import { notFound } from 'next/navigation';
+
+export default function SignupPage() {
+  notFound();
+  // This return is technically unreachable but good for linters/TS
+  return null;
+}
